a1bcec2a436a5ee74b7ec8fdb7f7fb00a6ba158e
Flouid <louiskeith99@gmail.com>
2022-04-12 17:01:31 -0400

    fixed bug, found another bug

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     encrypted_message = des.encrypt(message)
     print(f'encrypted message =\n\tlength:\t{len(encrypted_message)}\n\ttext:\t{encrypted_message}')
 
-    decrypted_message = des.decrypt(message)
+    decrypted_message = des.decrypt(encrypted_message)
     print(f'decrypted message =\n\tlength:\t{len(decrypted_message)}\n\ttext:\t{decrypted_message}')
 
 
